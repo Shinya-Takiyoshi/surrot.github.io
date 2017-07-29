2510efe7803ac09cdf38f89bf82f6b10e2e37c2e
iwasefy <setunatrip@yahoo.ne.jp>
2017-07-29 11:03:53 +0900

    Initial commit

--- rpg/php/mail.php
+++ rpg/php/mail.php
@@ -6,6 +6,11 @@ $to      = 'garo.savior@gmail.com';
 $subject = 'タイトル';
 $message = '本文';
 $headers = 'From: from@hoge.co.jp' . "\r\n";
+if(mb_send_mail($to, $subject, $message, $headers){
+echo "メールが送信されました";
+}else{
+echo "メールが送信できませんでした";
 
-mb_send_mail($to, $subject, $message, $headers);
+};
+
 ?>
